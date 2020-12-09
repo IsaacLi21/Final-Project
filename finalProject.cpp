ea3da8169dbc5cb63550e2d783a7d7072b53f49d
IsaacLi21 <74749585+IsaacLi21@users.noreply.github.com>
2020-12-09 12:20:55 -0700

    Add files via upload

--- finalProject.cpp
+++ finalProject.cpp
@@ -99,7 +99,6 @@ int main (int argc, const char * argv[]){
 
                 int distance=g.foundFlightDistance(start, end);
                 if (distance!=-1){
-                    cout<<"here"<<endl;
                     dist=to_string(distance);
                 }
                 else{
